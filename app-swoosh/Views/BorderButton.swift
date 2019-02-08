b495feba56758de44239412df391d9621d36d7c2
adiaz@mcclatchyservices.com <adiaz@mcclatchyservices.com>
2019-02-08 11:36:20 -0500

    basic layout welcome screen

---
+++ app-swoosh/Views/BorderButton.swift
@@ -0,0 +1,27 @@
+//
+//  BorderButton.swift
+//  app-swoosh
+//
+//  Created by Andres Diaz on 2/7/19.
+//  Copyright © 2019 Andres Diaz. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 2.0
+        layer.borderColor = UIColor.white.cgColor
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
