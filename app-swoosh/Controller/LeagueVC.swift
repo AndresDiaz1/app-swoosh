22500c33ab6267882a978470de69fedc6f858c98
adiaz@mcclatchyservices.com <adiaz@mcclatchyservices.com>
2019-02-08 16:45:11 -0500

    added validation before going to 3rd screen

--- app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/Controller/LeagueVC.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class LeagueVC: UIViewController {
     
+    @IBOutlet weak var nextBtn: BorderButton!
+    
+    var player: Player!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        player = Player()
         // Do any additional setup after loading the view.
     }
     
@@ -20,6 +24,23 @@ class LeagueVC: UIViewController {
         performSegue(withIdentifier: "SkillVCSegue", sender: self)
     }
 
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    func selectLeague(leagueType: String){
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+    
     /*
     // MARK: - Navigation
 
---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Andres Diaz on 2/8/19.
+//  Copyright © 2019 Andres Diaz. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLever: String?
+}
